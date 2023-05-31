7e9384f260257506c3a29b565e5737c506127abc
Nataliarm11 <nrequenam1@gmail.com>
2023-05-30 21:50:26 -0500

    V1:HomebankingPrueba

---
+++ src/main/java/com/mindhub/homebankingPrueba/HomebankingPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.mindhub.homebankingPrueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomebankingPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomebankingPruebaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mindhub/homebankingPrueba/HomebankingPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindhub.homebankingPrueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomebankingPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
